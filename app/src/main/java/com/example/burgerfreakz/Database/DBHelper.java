8099cc3aabfd6bfa80fb02b81d6e450b55e4ced7
MSI PC <navodinith@gmail.com>
2020-10-02 01:04:52 +0530

    Designs!!

--- app/src/main/java/com/example/burgerfreakz/Database/DBHelper.java
+++ app/src/main/java/com/example/burgerfreakz/Database/DBHelper.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
-
 import com.example.burgerfreakz.Classes.LoginD;
 import com.example.burgerfreakz.Classes.PDetails;
 import com.example.burgerfreakz.Classes.Riders;
